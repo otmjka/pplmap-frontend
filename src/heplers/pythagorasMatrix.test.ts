572333236c782f3374d4e22461dd89d82844592e
otmjka <otmjka@gmail.com>
2020-11-12 18:17:04 +0500

    feat: add tests for sum, countBy lodash substitute functions

--- src/heplers/pythagorasMatrix.test.ts
+++ src/heplers/pythagorasMatrix.test.ts
@@ -1,8 +1,19 @@
 import { sum, countBy } from './pythagorasMatrix';
 
-describe('123', () => {
-  test('simple', () => {
-    expect(true).toBe(true);
+describe('lodash substitute', () => {
+  test('[sum] function', () => {
+    const result: number = sum([2, 4, 15]);
+    expect(result).toBe(21);
+  });
+
+  test('[countBy] function', () => {
+    const result: Record<string, number> = countBy([2, 2, 1, 7, 3, 1, 1, 1, 7]);
+    expect(result).toEqual({
+      '1': 4,
+      '2': 2,
+      '3': 1,
+      '7': 2,
+    });
   });
 });
 
